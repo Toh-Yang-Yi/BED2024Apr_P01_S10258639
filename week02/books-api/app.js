3bc2023b66175ecc702a9e74c5b6591824a1500c
Toh-Yang-Yi <S10258639@connect.np.edu.sg>
2024-04-25 11:00:13 +0800

    completed part 3

--- week02/books-api/app.js
+++ week02/books-api/app.js
@@ -13,3 +13,44 @@ let books = [
  app.get('/books',(req,res) => {
     res.json(books);
  })
+ app.post('/books', (req,res) => {
+    const newBook = req.body;
+    newBook.id = books.length + 1
+    books.push(newBook);
+    res.status(201).json(newBook);
+ })
+ app.get('/books/:id', (req, res) =>{
+    const bookId = parseInt(req.params.id);
+    const book = books.find(book => book.id === bookId);
+
+    if (book) {
+        res.json(book);
+    } else {
+        res.status(404).send('Book not found');
+    }
+ });
+
+app.put('/books/:id', (req,res) => {
+    const bookId = parseInt(req.params.id);
+    const updatedBook = req.body;
+    const bookIndex = books.findIndex(book => book.id === bookId);
+
+    if (bookIndex !== -1) {
+        updatedBook.id = bookId;
+        books[bookIndex] = updatedBook;
+        res.json(updatedBook)
+    } else {
+        res.status(404).send('Book not found');
+    }
+})
+
+app.delete('/books/:id',(req,res) => {
+    const bookId = parseInt(req.params.id);
+    const bookIndex = books.findIndex(book => bookid === bookId);
+    if (bookIndex !== -1) {
+        books.splice(bookIndex, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send('Book not found');
+    }
+});
